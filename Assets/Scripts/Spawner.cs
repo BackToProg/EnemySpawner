29952310a6729cb5e67055c49faec3f79a6bdafd
Alexey Sutyagin <asutyagin.dev@gmail.com>
2023-09-25 13:26:36 +0300

    corrected codebase and styles

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,14 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SceneTemplate;
 using UnityEngine;
+using Random = System.Random;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _template;
-    
-    public void SpawnEnemy()
+    [SerializeField] private float _seconds;
+    [SerializeField] private Enemy _template;
+   
+    private GameObject[] _spawnPoints;
+    private readonly bool _isActive = true;
+
+    private void Start()
     {
-        Vector3 position = transform.position;
-        GameObject newObject = Instantiate(_template, new Vector3(position.x, 0, position.z), transform.rotation);
+        _spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+        var spawnOnRandomPointRun = StartCoroutine(SpawnOnRandomPoint());
     }
+
+    private IEnumerator SpawnOnRandomPoint()
+    {
+        while (_isActive)
+        {
+            int spawnIndex = GetRandomSpawnPointIndex();
+
+            GameObject spawnPoint = _spawnPoints[spawnIndex];
+            Vector3 spawnPointPosition = spawnPoint.transform.position;
+         
+            Instantiate(_template, new Vector3(spawnPointPosition.x, 0, 
+                spawnPointPosition.z), spawnPoint.transform.rotation);
+      
+            yield return new WaitForSeconds(_seconds);
+        }
+    }
+
+    private int GetRandomSpawnPointIndex()
+    {
+        int minSpawnPointIndex = 0;
+        int maxSpawnPointIndex = _spawnPoints.Length;
+        Random random = new Random();
+      
+        return random.Next(minSpawnPointIndex, maxSpawnPointIndex);
+    }
+
 }
